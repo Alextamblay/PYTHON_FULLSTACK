7c1f160643fe86776a843dba447015937505eb47
AlexTamblay <alexandratamblayiccp@gmail.com>
2023-04-21 10:14:10 -0400

    si

--- python/FUNDAMENTALS/hola_mundo.py
+++ python/FUNDAMENTALS/hola_mundo.py
@@ -2,7 +2,7 @@
 print("Hola, mundo" )
 # 2. imprime "Hola, Noelle" con el nombre en una variable
 name = "Noelle"
-print ("Hola "," name!")	# con una coma
+print ("Hola","name!")	# con una coma
 print("Hola "+" name!")	# con un +
 # 3. imprimir "Hola 42!" con el número en una variable
 name = 42
@@ -11,6 +11,6 @@ print("Hola "+" 42!")	# con una +	-- este debería arrojar un error!
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print("Amo comer fave_food1 y fave_food2" ) # con .format()
-print("tu código aquí" ) # con una cadena f
+print ("Amo comer ".format(fave_food1, fave_food2)"" )  # con .format() 
+print(F"Amo comer sushi y pizza" ) # con una cadena f
 
