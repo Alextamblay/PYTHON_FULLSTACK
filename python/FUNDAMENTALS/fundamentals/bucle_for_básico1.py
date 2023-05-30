0b9b1b47e9fd32def80eff19bed2794909288352
AlexTamblay <alexandratamblayiccp@gmail.com>
2023-05-30 09:05:13 -0400

    si

--- python/FUNDAMENTALS/fundamentals/bucle_for_básico1.py
+++ python/FUNDAMENTALS/fundamentals/bucle_for_básico1.py
@@ -4,18 +4,35 @@ for x in range(0, 151):
 for x in range(5,1001,5):
     print(x)
 
-for x in range(1,101):
-    if not x % 10:
-        print('coding dojo')
-    elif not x % 5:
-        print('coding')
+
+
+for i in range(1, 101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0:
+        print("Coding")
     else:
-        print(x)
+        print(i)
 
-sumando = 0
-for x in range(1, 500000, 2):
-    print(x)
-    
+sum = 0
+for x in range(1, 500001, 2):
+    sum += x
 
-for x in range(2018,1980,4):
-    print(x)
+print(f"La suma final es: {sum}")
+
+
+
+inicio = 2018
+
+while inicio > 0:
+    print(inicio)
+    inicio -= 4
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for num in range(lowNum, highNum + 1):
+    if num % mult == 0:
+        print(num)
+
