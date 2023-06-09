7e4ff3f55714920b57ca6061548f72a8e77b8b47
AlexTamblay <alexandratamblayiccp@gmail.com>
2023-06-09 10:06:39 -0400

    si

---
+++ python/FUNDAMENTALS/oop/primer_oop/mathdojo.py
@@ -0,0 +1,30 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num, *nums):
+        self.result += num
+        for x in nums:
+            self.result += x
+        return self
+
+    def subtract(self, num, *nums):
+        self.result -= num
+        for x in nums:
+            self.result -= x
+        return self
+
+
+# crear una instancia:
+md = MathDojo()
+
+# probar los métodos
+x = md.add(2).add(2, 5, 1).subtract(3, 2).result
+print(x)  # debería imprimir 5
+
+# ejecutar los métodos unas cuantas veces más y verificar el resultado
+x = md.add(10).add(5, 1).subtract(3, 2).result
+print(x)  # debería imprimir 18
+
+y = md.add(100, 200, 300).subtract(50, 25).result
+print(y)  # debería imprimir 538
