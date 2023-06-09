f643c47210663d5a1b5bbd6de52ddba93ce1a52a
AlexTamblay <alexandratamblayiccp@gmail.com>
2023-06-09 11:05:45 -0400

    si

--- python/FUNDAMENTALS/introduction/funciones_intermedias.py
+++ python/FUNDAMENTALS/introduction/funciones_intermedias.py
@@ -1,39 +1,29 @@
-'''x = [[5, 2, 3], [10, 8, 9]]
-estudiantes = [
-    {'first_name': 'Michael', 'last_name': 'Jordan'},
-    {'first_name': 'John', 'last_name': 'Rosales'}
-]
-directorio_deportes = {
-    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
-    'fútbol': ['Messi', 'Ronaldo', 'Rooney']
-}
-z = [{'x': 10, 'y': 20}]
+1.-"Actualizar valores en diccionarios y listas"
 
-# Cambia el valor 10 en x a 15
+x = [[5, 2, 3], [10, 8, 9]]
 x[1][0] = 15
-
-# Cambia el "apellido" del primer alumno de 'Jordan' a 'Bryant'
-estudiantes[0]['last_name'] = 'Bryant'
-
-# En el directorio_deportes, cambia "Messi" por "Andrés"
-directorio_deportes['fútbol'][0] = 'Andrés'
-
-# Cambia el valor 20 en z a 30
-
-z[0]['y'] = 30
-
-# Imprime los resultados después de los cambios
 print(x)
-print(estudiantes)
-print(directorio_deportes)
-print(z)'''
 
-'''def iterateDictionary(some_list):
+estudiantes = [{'first_name': 'Michael', 'last_name': 'Jordan'}, {'first_name': 'John', 'last_name': 'Rosales'}]
+estudiantes[0]['last_name'] = 'Bryant'
+print(estudiantes)
+
+directorio_deportes = {'basketball': ['Kobe', 'Jordan', 'James', 'Curry'], 'fútbol': ['Messi', 'Ronaldo', 'Rooney']}
+directorio_deportes['fútbol'][0] = 'Andrés'
+print(directorio_deportes)
+
+z = [{'x': 10, 'y': 20}]
+z[0]['y'] = 30
+print(z)
+
+2.-"Iterar a través de una lista de diccionarios"
+
+def iterateDictionary(some_list):
     for diccionario in some_list:
-        output = ""
         for clave, valor in diccionario.items():
-            output += f"{clave} - {valor}, "
-        print(output[:-2])  # Elimina la coma y el espacio extra al final
+            print(clave + " - " + valor)
+        print()
+
 
 estudiantes = [
     {'first_name': 'Michael', 'last_name': 'Jordan'},
@@ -41,10 +31,12 @@ estudiantes = [
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
+iterateDictionary(estudiantes)
 
-iterateDictionary(estudiantes)'''
 
-'''def iterateDictionary2(key_name, some_list):
+3.-"Obtener valores de una lista de diccionarios"
+
+def iterateDictionary2(key_name, some_list):
     for diccionario in some_list:
         if key_name in diccionario:
             print(diccionario[key_name])
@@ -55,8 +47,25 @@ estudiantes = [
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
-
 iterateDictionary2('first_name', estudiantes)
 print()
-iterateDictionary2('last_name', estudiantes)'''
+iterateDictionary2('last_name', estudiantes)
+
+4.-"Iterar a través de un diccionarios con valores de lista"
+
+def printInfo(some_dict):
+    for clave, valores in some_dict.items():
+        print(len(valores), clave.upper())
+        for valor in valores:
+            print(valor)
+        print()
+
+dojo = {
+'ubicaciones': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+'instructores': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+
+
+
 
